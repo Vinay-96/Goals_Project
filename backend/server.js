9e6424774944a37780fb9f672f55bc18e7fc0614
Vinay-96 <89307567+Vinay-96@users.noreply.github.com>
2022-04-12 11:50:19 +0530

    prepare deploy

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const colors = require("colors");
 const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
 const { createProxyMiddleware } = require("http-proxy-middleware");
+const cors = require("cors");
 const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
 
@@ -13,6 +14,8 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(cors())
 
 app.use("/api/goals", require("./routes/goalRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
